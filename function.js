688588ecced56bf925ba9aa8a9a45b13405f6738
Flavio Arzamendia <flavioarzamendia2019@gmail.com>
2023-01-27 14:24:32 -0300

    segundo commit

---
+++ function.js
@@ -0,0 +1,6 @@
+var getData = function (){
+  var  name = document.getElementById("nombre").value;
+  var email = document.getElementById("email").value;
+  var msg = document.getElementById("msg").value;
+  console.log(`${name} ${email} ${msg}`);
+}
